6a0c5b50da7991590f469998b294c8efd2b675f5
Bad coder <shaydebarra@gmail.com>
2016-02-22 22:10:01 +0000

    Update background.js
    Cleaned up code ..

--- Sleep_Override/background.js
+++ Sleep_Override/background.js
@@ -1,23 +1,20 @@
 
-var min = 0;
-var max = 1;
-var selected = ["sleep_active","sleep_disabled"]
-var current = min;
+var keepAwake = false;
 
 function updateIcon() {
-  chrome.browserAction.setIcon({path:"icon/" + selected[current] + ".png"});
 
-	if(current==max){
+	if(keepAwake){
+		chrome.browserAction.setIcon({path:"icon/sleep_disabled.png"});	
 		chrome.power.requestKeepAwake('display');
 		console.log("Keep awake: ON");
 	}else{
+		chrome.browserAction.setIcon({path:"icon/sleep_active.png"});	
 		chrome.power.releaseKeepAwake();
 		console.log("Keep awake: OFF"); 
 	}
-	  current++;
-  if (current > max)
-    current = min;
+	keepAwake = !keepAwake;
 }
 
 chrome.browserAction.onClicked.addListener(updateIcon);
 updateIcon();
+
